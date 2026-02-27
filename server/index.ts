bbb38e11cae3f56813460feb1be3301759315926
e4toy <rb885491@gmail.com>
2026-02-27 16:52:32 +0000

    Maybe the last

--- server/index.ts
+++ server/index.ts
@@ -34,15 +34,12 @@ function setupCors(app: express.Application) {
       origin?.startsWith("http://localhost:") ||
       origin?.startsWith("http://127.0.0.1:");
 
-    if (origin && (origins.has(origin) || isLocalhost)) {
-      res.header("Access-Control-Allow-Origin", origin);
-      res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS",
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      res.header("Access-Control-Allow-Credentials", "true");
-    }
+    if (!origin || origins.has(origin) || isLocalhost) {
+      res.header("Access-Control-Allow-Origin", origin || "*"); 
+          res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+          res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+          res.header("Access-Control-Allow-Credentials", "true");
+      }
 
     if (req.method === "OPTIONS") {
       return res.sendStatus(200);
